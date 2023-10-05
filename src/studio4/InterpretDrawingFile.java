056c8c8090098503b3112713472a5b17e6487f6f
Administrator <Administrator@PC-202302080932.dhcp.wustl.edu>
2023-10-05 10:52:34 -0500

    uncomplted

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,23 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String a= in.next();
+		
+		int[] Color= new int[3];
+	
+		for(int i=0;i<3;i++)
+		Color[i]= in.nextInt();
+		Color Color1 = new Color(Color[0],Color[1],Color[2]);
+		StdDraw.setPenColor(Color1);
+		Boolean b=in.nextBoolean();
+			
+		double x= in.nextDouble();
+		double y= in.nextDouble();
+		double halfWidth= in.nextDouble();
+		double halfHeight= in.nextDouble();
+			
+		StdDraw.rectangle(x, y, halfWidth, halfHeight);
 		
 	}
 }
